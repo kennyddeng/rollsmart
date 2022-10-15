815d7bf38dc7588c6c4802b51b4aed6ac15d7a19
Isabelle Bryenton <isabelle.bryenton@gmail.com>
2022-10-15 10:21:07 -0400

    break

--- breakpylint.py
+++ breakpylint.py
@@ -10,6 +10,11 @@ class BreakPylint:
     def __init__(self):
         print("i'm innit'")
         print("This line is way to long and it should definitely not pass the linter since it has more than 100 characters")
+        print("This line is way to long and it should definitely not pass the linter since it has more than 100 characters")
+        print("This line is way to long and it should definitely not pass the linter since it has more than 100 characters")
+        print("This line is way to long and it should definitely not pass the linter since it has more than 100 characters")
+        print("This line is way to long and it should definitely not pass the linter since it has more than 100 characters")
+        print("This line is way to long and it should definitely not pass the linter since it has more than 100 characters")
 
         print("This line is way to long and it should definitely not pass the linter since it has more than 100 characters")
         print("This line is way to long and it should definitely not pass the linter since it has more than 100 characters")
