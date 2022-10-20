884fcabc3402815751f8cc5090743c65ae19a11e
Isabelle Bryenton <isabelle.bryenton@gmail.com>
2022-10-20 11:58:07 -0400

    first draft of CLI wordart :)

---
+++ scripts/rollsmart.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+rollsmart.py is the main entry point script to interact with the rollsmart
+
+TO DO: Basically everything
+"""
+import fire
+from rich import print
+from rich.traceback import install; install()
+
+
+class Rollsmart:
+    def __init__(self):
+        """
+        Currently only initialized the CLI text art :)
+        """
+        self.initialize_cli()
+
+    def initialize_cli(self):
+        """
+        Prints CLI welcome message
+        """
+        print("[deep_sky_blue1 bold]                    _       _                                      _      ")
+        print("[deep_sky_blue1 bold]     _ _    ___     | |     | |     ___    _ __    __ _      _ _   | |_   ")
+        print("[deep_sky_blue1 bold]    | '_|  / _ \    | |     | |    (_-<   | '  \  / _` |    | '_|  |  _|  ")
+        print("[deep_sky_blue1 bold]   _|_|_   \___/   _|_|_   _|_|_   /__/_  |_|_|_| \__,_|   _|_|_   _\__|  ")
+        print('[dark_violet]    _|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""| ')
+        print("[dark_violet]   "'-O-O-'"'-O-O-'"'-O-O-'"'-O-O-'"'-O-O-'"'-O-O-'"'-O-O-'"'-O-O-'"'-O-O-')
+
+
+
+
+if __name__ == '__main__':
+    fire.Fire(Rollsmart)
