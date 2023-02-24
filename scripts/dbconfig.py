58d12d0b5476bea1355a5ee29442aa624118dd6a
Kennyy Deng <kennyddeng@gmail.com>
2023-02-24 09:05:45 -0500

    init: dbconfig.py

---
+++ scripts/dbconfig.py
@@ -0,0 +1,6 @@
+config = dict(
+    apiKey = "myapikey",
+    authDomain = "<myproject>.firebase.com",
+    databaseURL = "<myurl>.firebaseio.com/",
+    storageBucket = "<mystorage>.appspot.com"
+)
