a5d91c1d004f47227b77f8120e892937174b6bc7
Kennyy Deng <kennyddeng@gmail.com>
2023-02-26 10:48:34 -0500

    fix: sefix: maxref sensor name

---
+++ scripts/maxrefdes_117.py
@@ -0,0 +1,16 @@
+"""
+Heart Rate
+"""
+
+class MaxRefDes117():
+    def __init__(self):
+        self.gpioChannelA = 1
+
+    def get_raw_sensor_data(self):
+        # read sensor data from pins
+        return 42
+    
+    def get_processed_sensor_data(self):
+         # process raw sensor data into something useful to read
+        raw = self.get_raw_sensor_data()
+        return raw*10
