869a6dfe93e7563e31706a7df1393ef6ff0c976d
greatw1te2 <corbingarlough@cmail.carleton.ca>
2023-03-07 17:32:57 -0500

    Adding in local backup server

--- scripts/FBdataAdding.py
+++ scripts/FBdataAdding.py
@@ -10,13 +10,18 @@ import pyrebase
 
 # Create new Firebase config and database object
 config = {
-  "apiKey": "AIzaSyB8YyKlyoarYSiAfS6ZpbmfFHmW5xLIhYg",
-  "authDomain": "sysc4907rollsmart.firebaseapp.com",
-  "databaseURL": "https://sysc4907rollsmart-default-rtdb.firebaseio.com/",
-  "storageBucket": "sysc4907rollsmart.appspot.com"
+  'apiKey': "AIzaSyB8YyKlyoarYSiAfS6ZpbmfFHmW5xLIhYg",
+  'authDomain': "sysc4907rollsmart.firebaseapp.com",
+  'databaseURL': "https://sysc4907rollsmart-default-rtdb.firebaseio.com",
+  'projectId': "sysc4907rollsmart",
+  'storageBucket': "sysc4907rollsmart.appspot.com",
+  'messagingSenderId': "937699780579",
+  'appId': "1:937699780579:web:c626a608dc7a2f0b51a2d6",
+  'measurementId': "G-HHFNCNF4NP"
 }
 
 firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
 db = firebase.database()
 
 # define the names of tables used in Firebase database
@@ -28,6 +33,39 @@ jerkData = "jerk"
 seatData = "seat"
 speedData = "speed"
 weightDistData = "weightDistribution"
+
+def login():
+    print("Log in...")
+    email=input("Enter email: ")
+    password=input("Enter password: ")
+    try:
+        login = auth.sign_in_with_email_and_password(email, password)
+        print("Successfully logged in!")
+        # print(auth.get_account_info(login['idToken']))
+       # email = auth.get_account_info(login['idToken'])['users'][0]['email']
+       # print(email)
+    except:
+        print("Invalid email or password")
+    return
+
+#Signup Function
+
+def signup():
+    print("Sign up...")
+    email = input("Enter email: ")
+    password=input("Enter password: ")
+    try:
+        user = auth.create_user_with_email_and_password(email, password)
+        ask=input("Do you want to login?[y/n]")
+        if ask=='y':
+            login()
+    except: 
+        print("Email already exists")
+    return
+
+def addUserDB():
+	firebase.database().ref("users/" + firebase.auth().currentUser.uid)
+
 
 def addUserLogin(name, role, username, password):
 	db.child(loginInfo).child(name).child("role").set(role)
@@ -52,8 +90,21 @@ def addSpeedData(name, date, time, speed):
 def addWeightDistData(name, date, time, side):
 	db.child(collectedData).child(name).child(weightDistData).child(date).child(time).set(side)
 	
+#Main
 
+#ans=input("Are you a new user?[y/n]")
 
+#if ans == 'n':
+    #login()
+    
+#elif ans == 'y':
+    #signup()
+    
+#newName = input("Enter new name: ")
+#newRole = input("Enter new role: ")
+#newUsername = input("Enter new username: ")
+#newPassword = input("Enter new password: ")
+#addUserLogin(newName, newRole, newUsername, newPassword)
 #def main():
 
 	
---
+++ scripts/backupDB.py
@@ -0,0 +1,86 @@
+import sqlite3
+#import firebase_admin
+#from firebase_admin import credentials
+#from firebase_admin import db
+import requests
+import time
+from datetime import datetime
+from datetime import date
+import random
+from FBdataAdding import *
+
+
+UID = "4nIlD4s8Jdc2Uoa1q0DeONmmisH2"
+
+# Initializing Firebase credentials
+#cred = credentials.Certificate('/path/to/firebase/credentials.json')
+#firebase_admin.initialize_app(cred, {
+ #   'databaseURL': 'https://<your-database-url>.firebaseio.com'
+#})
+
+# Initializing SQLite connection
+sqlite_conn = sqlite3.connect('localDB.db')
+sqlite_cursor = sqlite_conn.cursor()
+
+# Define a function to send data to the Firebase database
+def send_to_firebase(data):
+	senName, val, time, date = data
+	print(("To Firebase -> senName: "+str(senName)+", val:"+str(val)+", date: "+str(date)+", time: " + str(time)))
+	if (senName == "heartRate"):
+		addHRData(UID, date, time, val)
+	elif (senName == "jerk"):
+		addJerkData(UID, date, time, val)
+	elif (senName == "seat"):
+		addSeatData(UID, date, time, val)
+	elif (senName == "speed"):
+		addSpeedData(UID, date, time, val)
+	elif (senName == "weightDistributuion"):
+		addWeightDistData(UID, date, time, val)
+	return True
+
+# Define a function to send data to the local SQLite database
+def send_to_sqlite(data):
+	senName, val, time, date = data
+	sqlite_cursor.execute("INSERT INTO sensor_data (sensor_name, value, timestamp, date) VALUES (?, ?, ?, ?)", data)
+	sqlite_conn.commit()
+	print("To SQLite-> senName: "+str(senName)+", val:"+str(val)+", date: "+str(date)+", time: " + str(time))
+
+def uploadData(sensor_data):
+	
+	# Check internet connection
+	internet_status = True
+	try:
+		requests.get('https://www.google.com')
+	except:
+		internet_status = False
+
+	# Send data to the appropriate database
+	if internet_status:
+        
+		if sqlite_cursor.execute("SELECT COUNT(*) FROM sensor_data").fetchone()[0] > 0:
+			data = sqlite_cursor.execute("SELECT * FROM sensor_data").fetchall()
+			print("===UPLOAD TO FB===")
+			for d in data:
+				send_to_firebase(d)
+			sqlite_cursor.execute("DELETE FROM sensor_data")
+			sqlite_conn.commit()
+			print("===END===")
+		for data in sensor_data:
+			send_to_firebase(data)
+	else:
+		for data in sensor_data:
+			send_to_sqlite(data)
+    
+# Continuously monitor the sensor data and send it to the appropriate database
+while True:
+	
+	now = datetime.now()
+	nowDate =  str(date.today())
+	nowTime = str(now.strftime("%H:%M:%S"))
+	
+	sensor_data = [('heartRate', random.randint(55,75), nowTime, nowDate),('speed', random.randint(0,10), nowTime, nowDate)]
+	uploadData(sensor_data)
+	time.sleep(5)
+	
+
+
