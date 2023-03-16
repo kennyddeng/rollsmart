c906bbe9379e13be3666d88046d3198d568ea2f0
Isabelle Bryenton <isabelle.bryenton@gmail.com>
2023-03-15 21:44:39 -0400

    add hr script

--- scripts/maxrefdes_117.py
+++ scripts/maxrefdes_117.py
@@ -1,16 +1,29 @@
 """
-Heart Rate
+Heart Rate Sensor : MAXREFDES117
+
+VIN --> 3.3V
+SDA --> I2C_SDA1 (pin3; GPIO 2)
+SLC --> I2C_SCL1 (pin5; GPIO 3)
+INT --> (pin7; GPIO 4)
+GND --> GND (pin9)
+
 """
+import max30102
+import hrcalc
 
 class MaxRefDes117():
     def __init__(self):
-        self.gpioChannelA = 1
+        self.gpioChannelA = 7
+        self.hr = max30102.MAX30102()
 
     def get_raw_sensor_data(self):
         # read sensor data from pins
-        return 42
-    
+        red, ir = self.hr.read_sequential()
+        return red, ir
+
     def get_processed_sensor_data(self):
          # process raw sensor data into something useful to read
-        raw = self.get_raw_sensor_data()
-        return raw*10
+        while True:
+            red, ir = self.get_raw_sensor_data()
+            hr = hrcalc.calc_hr_and_spo2(ir[:100], red[:100])
+        return hr
