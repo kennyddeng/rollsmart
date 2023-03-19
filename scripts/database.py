92ea0b528d599ff499bf8e4f5607b286050e9692
Isabelle Bryenton <isabelle.bryenton@gmail.com>
2023-03-19 13:12:29 -0400

    create function wrapper to push data

--- scripts/database.py
+++ scripts/database.py
@@ -2,18 +2,31 @@
 """
 RollSmart Database Interface
 """
+import sqlite3
 import firebase
+from datetime import datetime as dt
+
 import dbconfig
 
+USER_DATA = "collectedData"
+LOGIN_INFO = "loginInfo"
+SP02_DATA = "sp02"
+HR_DATA = "heartRate"
+JERK_DATA = "jerk"
+SEAT_DATA = "seat"
+SPEED_DATA = "speed"
+WEIGHT_DIST_DATA = "weightDistribution"
 
 class Database():
     """
     Class which initializes, pushes and manages Firebase operations for RollSmart
     """
-    def __init__(self):
+    def __init__(self, logger):
         """
         Initialize Remote and Local databases
         """
+        self.logger = logger
+
         # initialize Firebase cloud database
         self.firebase = pyrebase.initialize_app(dbconfig.config)
         self.db = self.firebase.database()
@@ -23,39 +36,45 @@ class Database():
 	    self.sqlite_cursor = sqlite_conn.cursor()
 
 
-    def push_sensor_data(self, entry, uuid, datatype, date, time, val):
+    def push_data(self, sensor):
         """
-        Push all sensor data to Firebase database.
+        Wrapper function which establishes which database is available for data pushing.
+        If internet is available, data will be pushed using Firebase ortherwise, data is stored
+        locally using local SQLite db.
 
-            Parameters:
-                    entry (str): Database entry type
-                    uuid (str): db_uuid of patient
-                    datatype (str): Name of sensor data type
-                    date (str): Calender date
-                    time (str): Calender time
-                    val (float): Sensor value to be pushed
-
-            Returns:
-                    none
+        When internet connection is available, and there is sensor data in local database, data
+        will get pushed
         """
+        def push_data_wrapper(f):
+            def wrapper(*args, **kwargs):
+                if self.check_internet_connection():
+                    # Check for data backlog in local database
+                    if sqlite_cursor.execute("SELECT COUNT(*) FROM sensor_data").fetchone()[0] > 0:
+                        data = sqlite_cursor.execute("SELECT * FROM sensor_data").fetchall()
+                        self.logger.log('DB: Local data backlog uploaded to Fyrebase')
+                        sqlite_cursor.execute("DELETE FROM sensor_data")
+                        sqlite_conn.commit()
 
-        sensor_data = [(datatype, val, time, date)]
-        backupDB.uploadData(sensor_data)
+                    self.db.child(USER_DATA).child(args[0]).child(sensor).child(args[1]).child(args[2]).set(args[3])
+                else:
+                    push_sqlite(sensor, args[0], args[1], args[2], args[3])
+                return f(*args, **kwargs)
+            return wrapper
+        return push_data_wrapper
 
-
-    def push_sqlite(self, sensor, value, time, date):
+    def push_sqlite(self, sensor, uuid, date, time, value):
         """
         Push to sqlite local database
         """
-        sqlite_cursor.execute("INSERT INTO sensor_data (sensor_name, value, timestamp, date) VALUES (?, ?, ?, ?)", data)
+        sqlite_cursor.execute("INSERT INTO sensor_data (sensor_name, value, timestamp, date) VALUES (?, ?, ?, ?)", [sensor, value, time, date])
         sqlite_conn.commit()
-        print(f"To SQLite-> senName: {senName}, val:{val}, date: {date}, time: {time}")
+        self.logger.log(f"SQLite: stored {sensor}, val:{value}, date: {date}, time: {time}")
 
 
     def check_internet_connection(self):
-    """
-    Check internet connection status
-    """
+        """
+        Check internet connection status
+        """
         internet_status = True
         try:
             requests.get('https://www.google.com')
@@ -63,6 +82,92 @@ class Database():
             internet_status = False
 
         return internet_status
+
+    @push_data(HR_DATA)
+    def add_hr_data(self, uuid, date, time, value):
+        """
+        Push heart rate data to database
+
+        Args:
+            uuid: Unique user idetification
+            date: date of measurement ('%Y-%m-%d')
+            time: time of measurement ('%H:%M:%S')
+            hr: heart rate value to push
+        """
+        self.db.child(USER_DATA).child(uuid).child(HR_DATA).child(date).child(time).set(hr)
+        self.logger.log("DB: Pushed HR data to database")
+
+    @push_data(SP02_DATA)
+    def add_sp02_data(self, uuid, date, time, sp02):
+        """
+        Push SP02 data to database
+
+        Args:
+            uuid: Unique user idetification
+            date: date of measurement ('%Y-%m-%d')
+            time: time of measurement ('%H:%M:%S')
+            sp02: sp02 value to push
+        """
+        self.logger.log("DB: Pushed SP02 data to database")
+
+    @push_data(JERK_DATA)
+    def add_imu_data(self, uuid, date, time, imu_val):
+       """
+        Push imu data to database
+        "JERK"
+
+        Args:
+            uuid: Unique user idetification
+            date: date of measurement ('%Y-%m-%d')
+            time: time of measurement ('%H:%M:%S')
+            imu_val: imu sensor values to push
+        """
+        self.db.child(USER_DATA).child(uuid).child(JERK_DATA).child(date).child(time).set(imu_val)
+        self.logger.log("DB: Pushed IMU jerk data to firebase")
+
+    @push_data(SEAT_DATA)
+    def add_seat_data(self, uuid, date, time, seat):
+        """
+        Push seat load cell data to database
+
+        Args:
+            uuid: Unique user idetification
+            date: date of measurement ('%Y-%m-%d')
+            time: time of measurement ('%H:%M:%S')
+            seat: seat load cell sensor values to push
+        """
+        self.db.child(USER_DATA).child(uuid).child(SEAT_DATA).child(date).child(time).set(seat)
+        self.logger.log("DB: Pushed seat data to firebase")
+
+    @push_data(SPEED_DATA)
+    def add_speed_data(self, uuid, date, time, speed):
+        """
+        Push speed data to database
+
+        Args:
+            uuid: Unique user idetification
+            date: date of measurement ('%Y-%m-%d')
+            time: time of measurement ('%H:%M:%S')
+            seat: seat load cell sensor values to push
+        """
+        self.db.child(USER_DATA).child(uuid).child(SPEED_DATA).child(date).child(time).set(speed)
+        self.logger.log("DB: Pushed speed data to firebase")
+
+    @push_data(WEIGHT_DIST_DATA)
+    def add_strain_data(self, uuid, date, time, side):
+        """
+        Push handlebar strain gauge data to database
+
+        WEIGHT DISTIBUTION
+        Args:
+            uuid: Unique user idetification
+            date: date of measurement ('%Y-%m-%d')
+            time: time of measurement ('%H:%M:%S')
+            seat: seat load cell sensor values to push
+        """
+        self.db.child(USER_DATA).child(uuid).child(WEIGHT_DIST_DATA).child(date).child(time).set(side)
+        self.logger.log("DB: Pushed strain gauge weight distribution data to firebase")
+
 
 
 if __name__ == '__main__':
--- scripts/rollsmart.py
+++ scripts/rollsmart.py
@@ -64,11 +64,6 @@ class Rollsmart:
         # rollator wheel diameter in metres
         self.wheel_diameter = 0.2
 
-        self.speed_interval_time_start = 0
-        self.speed_interval_time_end = 0
-        self.speed_interval_speed = 0
-        self.speed_interval_counter = 0
-
         # database
         self.db = Database()
 
@@ -128,8 +123,8 @@ class Rollsmart:
             self.logger.log(f' Load cell: value = {load_cell_val}')
 
             # check heart rate
-            heart_rate_val, hr_valid, sp02, sp02_valid = self.heart_rate.get_processed_sensor_data()
-            self.logger.log(f' HeartRate sensor:  HR = {load_cell_val}; '
+            hr_val, hr_valid, sp02, sp02_valid = self.heart_rate.get_processed_sensor_data()
+            self.logger.log(f' HeartRate sensor:  HR = {hr_val}; '
                             'SP02 = {sp02}; valid = {hr_valid}')
 
             # check imu
@@ -142,50 +137,12 @@ class Rollsmart:
             self.logger.log(f' Strain Guage: LEFT = {strain_left_val}')
             #self.logger.log(f' Strain Guage: RIGHT = {strain_right_val}')
 
-
             # push sensor data
-            self.push_sensor_data(self.db_entry, self.db_uuid, "jerk", creation_date, creation_time, imu_val)
-            self.push_sensor_data(self.db_entry, self.db_uuid, "seat", creation_date, creation_time, load_cell_val)
-            self.push_sensor_data_to_database(self.db_entry, self.db_uuid, "heartRate", creation_date, creation_time, heart_rate_val)
-            #self.push_sensor_data_to_database(self.db_entry, self.db_uuid, "weightDistribution", creation_date, creation_time, [strain_left_val, strain_right_val])
-            self.push_sensor_data_to_database(self.db_entry, self.db_uuid, "weightDistribution", creation_date, creation_time, strain_left_val)
-
-
-
-    def run_speed(self):
-        '''
-        speed sensor method.
-        '''
-        while self.running:
-            if self.speed.get_raw_sensor_data() == 1:
-                if self.speed_interval_counter == 0:
-                    # start time begins now
-                    self.speed_interval_time_start = time.time()
-                    if self.console_logging: print(datetime.now().isoformat(), ": speed sensor detect start time:", self.speed_interval_time_start)
-                self.speed_interval_counter += 1
-                if self.console_logging: print(datetime.now().isoformat(), ": incremented speed sensor detection counter", self.speed_interval_counter)
-
-            if self.speed_interval_counter == self.speed_samples_per_value:
-                # end time begins now
-                self.speed_interval_time_end = time.time()
-                if self.console_logging: print(datetime.now().isoformat(), ": speed sensor detect end time:", self.speed_interval_time_end)
-
-                # calc speed
-                self.speed_interval_speed = (3.14 * self.wheel_diameter * self.speed_samples_per_value) / (self.speed_interval_time_end - self.speed_interval_time_start)
-                if self.console_logging: print(datetime.now().isoformat(), ": speed sensor interval speed:", self.speed_interval_speed, "m/s")
-
-                # push speed to database
-                creation_date = datetime.today().strftime('%Y-%m-%d')
-                creation_time = datetime.today().strftime('%H:%M:%S')
-                self.push_sensor_data_to_database(self.db_entry, self.db_uuid, "speed", creation_date, creation_time, self.speed_interval_speed)
-                if self.console_logging: print(datetime.now().isoformat(), ": pushed speed sensor interval speed to database", self.speed_interval_speed)
-
-                # reset counter to 0
-                self.speed_interval_counter = 0
-                if self.console_logging: print(datetime.now().isoformat(), ": speed sensor detection counter reset,", self.speed_interval_counter)
-            time.sleep(self.speed_debounce_time)
-
-
+            seld.db.add_hr_data(self.db_uuid, creation_date, creation_time, hr_val, sp02)
+            self.db.add_imu_data(self.db_uuid, creation_date, creation_time, imu_val)
+            self.db.add_seat_data(self.db_uuid, creation_date, creation_time, load_cell_val)
+            self.db.add_strain_data(self.db_uuid, creation_date, creation_time, strain_left_val )
+            self.db.add_speed_data(self.db_uuid, creation_date, creation_time, speed_val)
 
 
     def initialize_cli(self):
